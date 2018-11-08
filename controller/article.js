20b3e38e20a54299f32b9507317d0e48d13f8bbc
Ailences <xs3646503@gmail.com>
2018-11-08 21:13:47 +0800

    完成了文章详情和文章编辑的功能

--- controller/article.js
+++ controller/article.js
@@ -1,5 +1,6 @@
 const moment = require('moment')
 const conn = require('../db/db.js')
+const marked = require('marked')
 
 module.exports = {
     handleArticleAddGet(req, res) {
@@ -35,9 +36,58 @@ module.exports = {
         })
     },
     handleArticleInfoGet(req, res) {
-        res.render('./articles/info.ejs', {
-            user: req.session.user,
-            isLogin: req.session.isLogin
+        // 获取传过来的文章id
+        const id = req.params.id
+        const querySql = 'select * from articles where id = ' + id
+        conn.query(querySql, (err, result) => {
+            // console.log(result)
+            const renderObj = {
+                user: req.session.user,
+                isLogin: req.session.isLogin
+            }
+            if (err || result.length !== 1) return res.render('./404.ejs', renderObj)
+            result[0].content = marked(result[0].content)
+            renderObj.article = result[0]
+            res.render('./articles/info.ejs', renderObj)
+            // console.log(renderObj)
+        })
+    },
+    handleArticleEditGet(req, res) {
+        if (!req.session.isLogin) return res.redirect('/')
+        const id = req.params.id
+        const querySql = 'select * from articles where id = ' + id
+        conn.query(querySql, (err, result) => {
+            if (err || result.length !== 1) return res.status(500).send({
+                status: 500,
+                msg: '文章获取失败!请重试!',
+                data: null
+            })
+            // console.log(result)
+            res.render('./articles/edit.ejs', {
+                user: req.session.user,
+                isLogin: req.session.isLogin,
+                article: result[0]
+            })
+        })
+    },
+    handleArticleEditPost(req, res) {
+        const article = req.body
+        article.ctime = moment().format('YYYY-MM-DD HH:mm:ss')
+        const updateSql = 'update articles set ? where id = ?'
+        // console.log(article)
+        conn.query(updateSql, [article, article.id], (err, result) => {
+            // console.log(err)
+            // console.log(result)
+            if (err || result.affectedRows !== 1) return res.status(400).send({
+                status: 400,
+                msg: '修改文章失败, 请重试!',
+                data: null
+            })
+            // 修改成功, 像客户端响应结果
+            res.send({
+                status: 200,
+                articleId: article.id
+            })
         })
     }
 }
--- router/article.js
+++ router/article.js
@@ -9,4 +9,7 @@ router.post('/article/add', ctrl.handleArticleAddPost)
 
 router.get('/article/info/:id', ctrl.handleArticleInfoGet)
 
+router.get('/article/edit/:id', ctrl.handleArticleEditGet)
+
+router.post('/article/edit', ctrl.handleArticleEditPost)
 module.exports = router
