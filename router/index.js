7947e9aca19f6cd05c50989995bd71cc0fa379e6
Ailences <xs3646503@gmail.com>
2018-11-09 18:52:15 +0800

    完成首页页面渲染,实现分页功能

--- router/index.js
+++ router/index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const router = express.Router()
 const ctrl = require('../controller/index.js')
 // 请求首页
-router.get('/', ctrl.showIndexPage)
+router.get('/', ctrl.handleIndexGet)
 // 把路由对象暴露出去
 module.exports = router
